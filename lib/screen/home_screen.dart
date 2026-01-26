689be5afa7fb93893e883be00328e355dc741e93
taixii <daniel1616@naver.com>
2026-01-26 23:14:39 +0900

    feat: update calendar design

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,13 +1,81 @@
+import 'package:calendar_scheduler/const/color.dart';
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
+  State<HomeScreen> createState() => _HomeScreenState();
+}
 
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime? selectedDay;
+
+  @override
+  Widget build(BuildContext context) {
+    final defaultBoxDecoration = BoxDecoration(
+      borderRadius: BorderRadius.circular(6.0),
+      border: Border.all(
+        color: Colors.grey[200]!,
+        width: 1.0,
+      ),
+    );
+
+    final defaultTextStyle = TextStyle(
+      color: Colors.grey[600],
+      fontWeight: FontWeight.w700,
+    );
+
+    return Scaffold(
+      body: SafeArea(
+        child: TableCalendar(
+          focusedDay: DateTime(2026, 1),
+          firstDay: DateTime(1800),
+          lastDay: DateTime(3000),
+          headerStyle: HeaderStyle(
+            titleCentered: true,
+            formatButtonVisible: false,
+            titleTextStyle: TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          calendarStyle: CalendarStyle(
+            isTodayHighlighted: true,
+            defaultDecoration: defaultBoxDecoration,
+            weekendDecoration: defaultBoxDecoration,
+            selectedDecoration: defaultBoxDecoration.copyWith(
+              border: Border.all(
+                color: primaryColor,
+              ),
+            ),
+            todayDecoration: defaultBoxDecoration.copyWith(
+              color: primaryColor,
+            ),
+            outsideDecoration: defaultBoxDecoration.copyWith(
+              border: Border.all(
+                color: Colors.transparent,
+              )
+            ),
+            defaultTextStyle: defaultTextStyle,
+            weekendTextStyle: defaultTextStyle,
+            selectedTextStyle: defaultTextStyle.copyWith(
+              color: primaryColor,
+            ),
+          ),
+
+          onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
+            setState(() {
+              this.selectedDay = selectedDay;
+            });
+          },
+          selectedDayPredicate: (DateTime date) {
+            // 현재 화면상 모든 날짜를 date로 받음
+            if (selectedDay == null) return false;
+            return date.isAtSameMomentAs(selectedDay!);
+          },
+        ),
       ),
     );
   }
