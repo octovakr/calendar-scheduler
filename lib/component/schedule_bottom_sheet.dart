831e7bc4c178462e50380ab7983999f718e4ee7e
taixii <daniel1616@naver.com>
2026-01-29 18:18:50 +0900

    refactor: lift onSaved and validate handlers of bottom sheet to parent widget

--- lib/component/schedule_bottom_sheet.dart
+++ lib/component/schedule_bottom_sheet.dart
@@ -20,90 +20,117 @@ class _ScheduleBottomSheetState extends State<ScheduleBottomSheet> {
       child: SafeArea(
         child: Padding(
           padding: EdgeInsetsGeometry.fromLTRB(8, 16, 8, 0),
-          child: Column(
-            children: [
-              _Time(),
-              SizedBox(height: 8),
-              _Content(),
-              SizedBox(height: 8),
-              _Categories(
-                selectedColor: selectedColor,
-                onTap: (String color){
-                  setState(() {
-                    selectedColor = color;
-                  });
-                },
-              ),
-              SizedBox(height: 8),
-              _SaveButton(),
-            ],
+          child: Form(
+            child: Column(
+              children: [
+                _Time(
+                  onStartSaved: onStartTimeSaved,
+                  onStartValidate: onStartTimeValidate,
+                  onEndSaved: onEndTimeSaved,
+                  onEndValidate: onEndTimeValidate,
+                ),
+                SizedBox(height: 8),
+                _Content(
+                  onSaved: onContentSaved,
+                  validator: onContentValidate,
+                ),
+                SizedBox(height: 8),
+                _Categories(
+                  selectedColor: selectedColor,
+                  onTap: (String color){
+                    setState(() {
+                      selectedColor = color;
+                    });
+                  },
+                ),
+                SizedBox(height: 8),
+                _SaveButton(),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
+
+  void onStartTimeSaved(String? val) {
+
+  }
+
+  String? onStartTimeValidate(String? val) {
+
+  }
+
+  void onEndTimeSaved(String? val) {
+
+  }
+
+  String? onEndTimeValidate(String? val) {
+
+  }
+
+  void onContentSaved(String? val) {
+
+  }
+
+  String? onContentValidate(String? val) {
+
+  }
 }
 
 class _Time extends StatelessWidget {
   // 이 프로젝트 전체에 global하게 단 하나만 존재하는 key 값을 만들어서 그 안에 form의 상태를 저장.
-  final GlobalKey<FormState> formKey = GlobalKey(); // 공식처럼 알아두기.
+  // final GlobalKey<FormState> formKey = GlobalKey(); // 공식처럼 알아두기.
+  final FormFieldSetter<String> onStartSaved;
+  final FormFieldSetter<String> onEndSaved;
+  final FormFieldValidator<String> onStartValidate;
+  final FormFieldValidator<String> onEndValidate;
 
-  _Time({super.key});
+  _Time({
+    required this.onStartSaved,
+    required this.onEndSaved,
+    required this.onStartValidate,
+    required this.onEndValidate,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Expanded(
-                  child: CustomTextField(
-                    label: '시작 시간',
-                    onSaved: (String? val){
-
-                    },
-                    validator: (String? val){
-                      print('start time validate');
-                      return 'start time error!!!!!';
-                    },
-                  ),
-              ),
-              SizedBox(width: 16.0),
-              Expanded(
-                  child: CustomTextField(
-                    label: '마감 시간',
-                    onSaved: (String? val){
-
-                    },
-                    validator: (String? val){
-                      print('end time validate');
-                      return 'end time error!!!!!';
-                    },
-                  ),
-              ),
-            ],
-          ),
-          ElevatedButton(
-              onPressed: (){
-                final validated = formKey.currentState!.validate();
-                // formKey를 넣어준 form 내부의 텍스트필드 위젯에서
-                // validator 파라미터에 들어간 함수가 String을 반환하는 경우가 하나라도 있으면
-                // formKey.currentState.validate()는 false를 반환함. (검증 안됨)
-                print('-----validated-----');
-                print(validated);
-              },
-              child: Text('save'),
-          ),
-        ],
-      ),
+    return Column(
+      children: [
+        Row(
+          children: [
+            Expanded(
+                child: CustomTextField(
+                  label: '시작 시간',
+                  onSaved: onStartSaved,
+                  validator: onStartValidate,
+                ),
+            ),
+            SizedBox(width: 16.0),
+            Expanded(
+                child: CustomTextField(
+                  label: '마감 시간',
+                  onSaved: onEndSaved,
+                  validator: onEndValidate,
+                ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
 
 class _Content extends StatelessWidget {
-  const _Content({super.key});
+  final FormFieldSetter<String> onSaved;
+  final FormFieldValidator<String> validator;
+
+  const _Content({
+    required this.onSaved,
+    required this.validator,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -111,12 +138,8 @@ class _Content extends StatelessWidget {
       child: CustomTextField(
         label: '내용',
         expand: true,
-        onSaved: (String? val){
-
-        },
-        validator: (String? val){
-
-        },
+        onSaved: onSaved,
+        validator: validator,
       ),
     );
   }
